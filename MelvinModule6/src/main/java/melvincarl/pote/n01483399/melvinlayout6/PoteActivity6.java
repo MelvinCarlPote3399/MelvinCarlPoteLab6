c12cc92571b6f1eb17b2bb1453d11d8fe5345a3d
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-14 00:42:43 -0400

    Initial commit

---
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/PoteActivity6.java
@@ -0,0 +1,14 @@
+package melvincarl.pote.n01483399.melvinlayout6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PoteActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
