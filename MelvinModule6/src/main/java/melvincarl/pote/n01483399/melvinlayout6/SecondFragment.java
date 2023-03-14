edab9f520671b6e7d592b408697cd05478fa159f
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-14 14:36:55 -0400

    Created the remaining fragments

--- MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/SecondFragment.java
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/SecondFragment.java
@@ -3,26 +3,27 @@ package melvincarl.pote.n01483399.melvinlayout6;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import java.io.*;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import java.io.*;
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment #newInstance} factory method to
+ * Use the {@link SecondFragment #newInstance} factory method to
  * create an instance of this fragment.
  */
 
-public class FirstFragment extends Fragment {
 
-    public FirstFragment(){
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
         // require a empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 }
--- MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/ThirdFragment.java
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/ThirdFragment.java
@@ -3,26 +3,28 @@ package melvincarl.pote.n01483399.melvinlayout6;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import java.io.*;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment #newInstance} factory method to
+ * Use the {@link ThirdFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 
-public class FirstFragment extends Fragment {
+import java.io.*;
 
-    public FirstFragment(){
+public class ThirdFragment extends Fragment {
+
+    public ThirdFragment(){
         // require a empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 }
