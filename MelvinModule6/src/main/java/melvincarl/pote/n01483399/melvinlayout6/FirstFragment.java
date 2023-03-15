b681189b7e2bc17075beeed95c1d11e0d982a896
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-15 01:03:59 -0400

    Completed the first fragment.

--- MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/FirstFragment.java
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/FirstFragment.java
@@ -1,5 +1,6 @@
 package melvincarl.pote.n01483399.melvinlayout6;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import java.io.*;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +18,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 
+
 public class FirstFragment extends Fragment {
+    ImageButton imageButton;
+    int counter = 0;
+    //Context context = getApplicationContext();
+    ProgressBar simpleProgressBar;
+    int progress = 0;
 
     public FirstFragment(){
         // require a empty public constructor
@@ -22,7 +32,83 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        simpleProgressBar = (ProgressBar)view.findViewById(R.id.progressBar);
+        setProgressValue(progress);
+        imageButton = (ImageButton)view.findViewById(R.id.imageButton3);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //setProgressValue(progress);
+
+                if (counter == 0){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+                }
+
+                else if (counter == 1){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+
+                }
+
+                else if (counter == 2){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+
+                }
+                else if(counter == 3){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+
+                }
+                else if(counter == 4){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+
+                }
+                else if(counter == 5){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+                    simpleProgressBar.setVisibility(View.INVISIBLE);
+                }
+                else if (counter == 6){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+                }
+                else if (counter == 7){
+                    counter++;
+                    Toast.makeText(getContext(), getString(R.string.toast_text) + String.valueOf(counter),Toast.LENGTH_LONG).show();
+                }
+                else if (counter == 8){
+                    System.exit(0);
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
+
     }
+
+    private void setProgressValue(int progress) {
+        // set the progress
+        simpleProgressBar.setProgress(progress);
+        // thread is used to change the progress value
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setProgressValue(progress + 10);
+            }
+        });
+        thread.start();
+    }
+
+
 }
+
