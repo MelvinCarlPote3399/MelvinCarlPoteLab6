fb0c5665d6cb2e7a27c7c789060462cef66d8f40
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-16 03:08:36 -0400

    Created on-click listener for the third fragment page; added in the highlight colour, for when an item from the list is selected.

--- MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/ThirdFragment.java
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/ThirdFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +21,8 @@ import java.io.*;
 
 public class ThirdFragment<provinces_and_territories> extends Fragment {
 
+    private ListView listView;
+
     public ThirdFragment(){
         // require a empty public constructor
     }
@@ -25,6 +30,18 @@ public class ThirdFragment<provinces_and_territories> extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_third, container, false);
+        listView = view.findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.provinces_and_territories));
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                listView.setSelector(android.R.color.holo_blue_dark);
+            }
+        });
+
 
         return view;
     }
