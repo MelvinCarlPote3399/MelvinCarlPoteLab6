5c940a4d4e9975252fdaa022036ad0b2ff44b6d9
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-16 03:08:36 -0400

    Managed to implement passing in the string to the fourth fragment.

--- MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/FourthFragment.java
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/FourthFragment.java
@@ -2,11 +2,14 @@ package melvincarl.pote.n01483399.melvinlayout6;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +27,18 @@ public class FourthFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fourth, container, false);
+        TextView provincesView = view.findViewById(R.id.fourthFragment);
 
-        return inflater.inflate(R.layout.fragment_fourth, container, false);
+        getParentFragmentManager().setFragmentResultListener("requestResult", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                provincesView.setText(result.getString("selectPro"));
+            }
+        });
+
+        //Bundle bundle = getArguments();
+
+        return view;
     }
 }
--- MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/ThirdFragment.java
+++ MelvinModule6/src/main/java/melvincarl/pote/n01483399/melvinlayout6/ThirdFragment.java
@@ -3,10 +3,15 @@ package melvincarl.pote.n01483399.melvinlayout6;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +23,8 @@ import java.io.*;
 
 public class ThirdFragment<provinces_and_territories> extends Fragment {
 
+    private ListView listView;
+
     public ThirdFragment(){
         // require a empty public constructor
     }
@@ -25,6 +32,38 @@ public class ThirdFragment<provinces_and_territories> extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_third, container, false);
+        listView = view.findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.provinces_and_territories));
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                listView.setSelector(android.R.color.holo_blue_dark);
+
+                Bundle bundle = new Bundle();
+
+                String selected = adapterView.getItemAtPosition(position).toString();
+
+                String province = getResources().getStringArray(R.array.provinces_and_territories)[position];
+
+                bundle.putString("selectitem",selected);
+                bundle.putString("selectPro",province);
+                getParentFragmentManager().setFragmentResult("requestResult",bundle);
+                /*
+                FourthFragment fragObject = new FourthFragment();
+                fragObject.setArguments(bundle);
+
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainerView2,fragObject);
+                fragmentTransaction.commit();
+                */
+
+            }
+        });
+
 
         return view;
     }
